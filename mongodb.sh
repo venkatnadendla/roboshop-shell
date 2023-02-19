943e9f970a1ad1f882360c89ba9de6aae894e613
venkat <ramana.nadela@gmail.com>
2023-02-19 23:31:45 +0530

    code modifiedd

--- mongodb.sh
+++ mongodb.sh
@@ -2,5 +2,4 @@ cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
-
 ## update the mongod.conf file 127.0.0.1 wit 0.0.0.0
