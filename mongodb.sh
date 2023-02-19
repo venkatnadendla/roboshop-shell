84756fb68e0d0dc807fb82fe4ade1581a90691f4
venkat <ramana.nadela@gmail.com>
2023-02-19 23:12:27 +0530

    roboshop components

--- mongodb.sh
+++ mongodb.sh
@@ -1,1 +1,6 @@
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org -y
+systemctl enable mongod
+systemctl start mongod
+
+## update the mongod.conf file 127.0.0.1 wit 0.0.0.0
